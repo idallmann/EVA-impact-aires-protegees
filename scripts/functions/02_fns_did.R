d1f3d511ec4c2abb3ff1b3825d65e4911f28145f
vuillota <vuillota@afd.fr>
2023-08-16 09:16:31 +0000

    Pursue DiD scripts

--- scripts/functions/02_fns_did.R
+++ scripts/functions/02_fns_did.R
@@ -16,14 +16,34 @@ fn_did_list_pa = function(iso)
 }
 
 
-fn_did_load_df_matched_wide = function(iso, wdpaid, ext_output)
+fn_did_load_df = function(iso, wdpaid, ext_output)
 {
+  #Load matched datasets
   df_matched_wide = s3read_using(data.table::fread,
                                  object = paste0("data_tidy/mapme_bio_data/matching", "/", iso, "/", wdpaid, "/", paste0("matched_wide", "_", iso, "_", wdpaid, ext_output)),
                                  bucket = "projet-afd-eva-ap",
                                  opts = list("region" = "")) %>%
-    select(c(wdpaid, group, status_yr, starts_with("treecover"), starts_with("treeloss")))
+    select(c(direction_regionale, pays, iso3, wdpaid, group, status_yr, annee_octroi, starts_with("treecover"), starts_with("treeloss")))
   
-  return(df_matched_wide)
+  df_unmatched_wide = s3read_using(data.table::fread,
+                                 object = paste0("data_tidy/mapme_bio_data/matching", "/", iso, "/", wdpaid, "/", paste0("unmatched_wide", "_", iso, "_", wdpaid, ext_output)),
+                                 bucket = "projet-afd-eva-ap",
+                                 opts = list("region" = "")) %>%
+    select(c(direction_regionale, pays, iso3, wdpaid, group, status_yr, annee_octroi, starts_with("treecover"), starts_with("treeloss")))
+  
+  df_matched_long = s3read_using(data.table::fread,
+                                 object = paste0("data_tidy/mapme_bio_data/matching", "/", iso, "/", wdpaid, "/", paste0("matched_long", "_", iso, "_", wdpaid, ext_output)),
+                                 bucket = "projet-afd-eva-ap",
+                                 opts = list("region" = "")) %>%
+    select(c(direction_regionale, iso3, wdpaid, group, status_yr, annee_octroi, year, var, fc_ha))
+  
+  df_unmatched_long = s3read_using(data.table::fread,
+                                 object = paste0("data_tidy/mapme_bio_data/matching", "/", iso, "/", wdpaid, "/", paste0("unmatched_long", "_", iso, "_", wdpaid, ext_output)),
+                                 bucket = "projet-afd-eva-ap",
+                                 opts = list("region" = "")) %>%
+    select(c(direction_regionale, iso3, wdpaid, group, status_yr, annee_octroi, year, var, fc_ha))
+  
+  return(list("df_matched_wide" = df_matched_wide, "df_unmatched_wide" = df_unmatched_wide,
+              "df_matched_long" = df_matched_long, "df_unmatched_long" = df_unmatched_long))
 
 }
