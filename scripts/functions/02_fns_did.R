4ae08e2ecc8e195525c98e78f1fda9af78c4b59c
vuillota <vuillota@afd.fr>
2023-08-31 16:49:48 +0000

    Update

--- scripts/functions/02_fns_did.R
+++ scripts/functions/02_fns_did.R
@@ -5,41 +5,41 @@
 ### yr_min : the minimum for treatment year
 ##OUTPUTS :
 ### list_pa : a dataframe with the PA matched
-fn_did_list_pa = function(iso, save_dir)
+fn_did_list_pa = function(iso, load_dir)
 {
   list_pa = s3read_using(data.table::fread,
                     bucket = "projet-afd-eva-ap",
-                    object = paste(save_dir, iso, paste0("list_pa_matched_", iso, ".csv"), sep = "/"),
+                    object = paste(load_dir, iso, paste0("list_pa_matched_", iso, ".csv"), sep = "/"),
                     opts = list("region" = "")) 
 
   return(list_pa)
 }
 
 
-fn_did_load_df = function(iso, wdpaid, save_dir, ext_output)
+fn_did_load_df = function(iso, wdpaid, load_dir, ext_input)
 {
   #Load matched datasets
   df_matched_wide = s3read_using(data.table::fread,
-                                 object = paste0(save_dir, "/", iso, "/", wdpaid, "/", paste0("matched_wide", "_", iso, "_", wdpaid, ext_output)),
+                                 object = paste0(load_dir, "/", iso, "/", wdpaid, "/", paste0("matched_wide", "_", iso, "_", wdpaid, ext_input)),
                                  bucket = "projet-afd-eva-ap",
                                  opts = list("region" = "")) %>%
     select(c(region, country_en, iso3, wdpaid, group, assetid, status_yr, year_funding_first, year_funding_all, res_m, starts_with("treecover"), starts_with("treeloss")))
   
   df_unmatched_wide = s3read_using(data.table::fread,
-                                 object = paste0(save_dir, "/", iso, "/", wdpaid, "/", paste0("unmatched_wide", "_", iso, "_", wdpaid, ext_output)),
+                                 object = paste0(load_dir, "/", iso, "/", wdpaid, "/", paste0("unmatched_wide", "_", iso, "_", wdpaid, ext_input)),
                                  bucket = "projet-afd-eva-ap",
                                  opts = list("region" = "")) %>%
     select(c(region, country_en, iso3, wdpaid, group, assetid, status_yr, year_funding_first, year_funding_all, res_m, starts_with("treecover"), starts_with("treeloss")))
   
   df_matched_long = s3read_using(data.table::fread,
-                                 object = paste0(save_dir, "/", iso, "/", wdpaid, "/", paste0("matched_long", "_", iso, "_", wdpaid, ext_output)),
+                                 object = paste0(load_dir, "/", iso, "/", wdpaid, "/", paste0("matched_long", "_", iso, "_", wdpaid, ext_input)),
                                  bucket = "projet-afd-eva-ap",
                                  opts = list("region" = "")) %>%
     select(c(region, iso3, wdpaid, group, assetid, status_yr, year_funding_first, year_funding_all, res_m, year, var, fc_ha))
     #select(c(region, country_en, iso3, wdpaid, group, status_yr, year_funding_first, year_funding_all, year, var, fc_ha))
   
   df_unmatched_long = s3read_using(data.table::fread,
-                                 object = paste0(save_dir, "/", iso, "/", wdpaid, "/", paste0("unmatched_long", "_", iso, "_", wdpaid, ext_output)),
+                                 object = paste0(load_dir, "/", iso, "/", wdpaid, "/", paste0("unmatched_long", "_", iso, "_", wdpaid, ext_input)),
                                  bucket = "projet-afd-eva-ap",
                                  opts = list("region" = "")) %>%
     select(c(region, iso3, wdpaid, group, assetid, status_yr, year_funding_first, year_funding_all, year, res_m, var, fc_ha))
@@ -62,7 +62,7 @@ fn_did_load_df = function(iso, wdpaid, save_dir, ext_output)
 ## OUTPUTS
 ### None
 
-fn_did_te = function(iso, wdpaid, df_long_m, alpha, save_dir, ext_output)
+fn_did_att = function(iso, wdpaid, df_long_m, alpha, save_dir)
 {
   
   #First extract some relevant variables
@@ -110,7 +110,7 @@ fn_did_te = function(iso, wdpaid, df_long_m, alpha, save_dir, ext_output)
            .after = status_yr) %>%
     group_by(assetid) %>%
     mutate(FL_2000_cum = (fc_ha-fc_ha[year == 2000])/fc_ha[year == 2000]*100,
-           fper = fc_ha/res_ha*100) %>%
+           fc_2000 = fc_ha[year == 2000]) %>%
     ungroup()
     
   #Compute dynamic TE with did package. 
@@ -161,57 +161,14 @@ fn_did_te = function(iso, wdpaid, df_long_m, alpha, save_dir, ext_output)
              wdpaid = wdpaid,
              .before = "treatment_year")
     
-    
-    ##For deforestation
-    ### ATT computation
-    fl_attgt = did::att_gt(yname = "FL_2000_cum",
-                           gname = "treatment_year",
-                           idname = "assetid",
-                           tname = "year",
-                           control_group = "nevertreated", #Thsi corresponds to control pixels as defined in the matching , with treatment year set to 0
-                           xformla = ~1,
-                           alp = alpha, #For 95% confidence interval
-                           allow_unbalanced_panel = TRUE, #Ensure no unit is dropped, though every pixel should have data for all years in the period
-                           bstrap=TRUE, #Compute bootstrap CI
-                           biters = 1000, #The number of bootstrap iteration, 1000 is default
-                           cband = TRUE, #Compute CI
-                           clustervars = NULL, #No clustering seems relevant to me 
-                           base_period = "varying",
-                           data = df_did,
-                           print_details = F)
-    
+    ###Plot results
     fig_fc_att = did::ggdid(fc_attgt,
-                            ylab = "Avoided deforestation (ha)",
-                            title = "Treatment effect of the conservation",
-                            grtitle = paste("WDPA ID", wdpaid, "in", country.iso, "implemented in"),
-                            theming = TRUE)
-    
-    ### Report results in a dataframe
-    ### confidence intervals are computed from bootstrap standard errors after a coefficient is applied.
-    ### This computation takes the one from did:::summary.MP function, line 15 and 16.
-    df_fl_attgt = data.frame("treatment_year" = fl_attgt$group,
-                             "year" = fl_attgt$t,
-                             "att" = fl_attgt$att,
-                             "c" = fl_attgt$c,
-                             "se" = fl_attgt$se,
-                             "n" = fl_attgt$n) %>%
-      mutate(cband_lower = round(att-c*se, 4),
-             cband_upper = round(att+c*se, 4),
-             sig = sign(cband_lower) == sign(cband_upper)) %>%
-      rename_with(.cols = c(cband_lower, cband_upper, sig),
-                  .fn = \(x) paste0(x, "_", gsub("0.", "", 1-alpha))) %>%
-      mutate(region = region.name,
-             iso3 = country.iso,
-             wdpaid = wdpaid,
-             .before = "treatment_year")
-    
-    fig_fl_att = did::ggdid(fl_attgt,
-                      grtitle = NULL,
-                      theming = TRUE) %>%
-      + labs(title = "Treatment effect of the conservation",
+                            grtitle = NULL,
+                            theming = TRUE) %>%
+      + labs(title = "Average treatment effect on the treated (ATT) of the conservation program",
              subtitle = paste("WDPA ID", wdpaid, "in", country.iso, "implemented in", treatment.year),
-             caption = "Treatment effect is interpreted as the deforestation avoided thanks to the conservation program, in percentage points (p.p).\nFor instance if deforestation rate relative to 2000 is 0% before treatment, 1% in treated group after 5 years and 5% in control group after five years, then the treatment effect is 4 p.p.",
-             y = "Avoided deforestation (pp)") %>%
+             caption = "Treatment effect is interpreted as the deforestation avoided thanks to the conservation program, hectare (ha).\nA negative effect means the conservation program is associated with higher deforestation.",
+             y = "Avoided deforestation (ha)") %>%
       + theme(
         axis.text.x = element_text(angle = -20, hjust = 0.5, vjust = 0.5),
         axis.text=element_text(size=11, color = "black"),
@@ -238,6 +195,112 @@ fn_did_te = function(iso, wdpaid, df_long_m, alpha, save_dir, ext_output)
       )
     
     
+    
+    ##For deforestation
+    ### ATT computation
+    fl_attgt = did::att_gt(yname = "FL_2000_cum",
+                           gname = "treatment_year",
+                           idname = "assetid",
+                           tname = "year",
+                           control_group = "nevertreated", #Thsi corresponds to control pixels as defined in the matching , with treatment year set to 0
+                           xformla = ~1,
+                           alp = alpha, #For 95% confidence interval
+                           allow_unbalanced_panel = TRUE, #Ensure no unit is dropped, though every pixel should have data for all years in the period
+                           bstrap=TRUE, #Compute bootstrap CI
+                           biters = 1000, #The number of bootstrap iteration, 1000 is default
+                           cband = TRUE, #Compute CI
+                           clustervars = NULL, #No clustering seems relevant to me 
+                           base_period = "varying",
+                           data = df_did,
+                           print_details = F)
+    
+
+    ### Report results in a dataframe
+    ### confidence intervals are computed from bootstrap standard errors after a coefficient is applied.
+    ### This computation takes the one from did:::summary.MP function, line 15 and 16.
+    df_fl_attgt = data.frame("treatment_year" = fl_attgt$group,
+                             "year" = fl_attgt$t,
+                             "att" = fl_attgt$att,
+                             "c" = fl_attgt$c,
+                             "se" = fl_attgt$se,
+                             "n" = fl_attgt$n) %>%
+      mutate(cband_lower = round(att-c*se, 4),
+             cband_upper = round(att+c*se, 4),
+             sig = sign(cband_lower) == sign(cband_upper)) %>%
+      rename_with(.cols = c(cband_lower, cband_upper, sig),
+                  .fn = \(x) paste0(x, "_", gsub("0.", "", 1-alpha))) %>%
+      #Compute time relative to treatment year
+      mutate(time = year - treatment_year,
+             .before = year) %>%
+      mutate(region = region.name,
+             iso3 = country.iso,
+             wdpaid = wdpaid,
+             .before = "treatment_year")
+    
+    fig_fl_att = did::ggdid(fl_attgt,
+                      grtitle = NULL,
+                      theming = TRUE) %>%
+      + labs(title = "Average treatment effect on the treated (ATT) of the conservation program",
+             subtitle = paste("WDPA ID", wdpaid, "in", country.iso, "implemented in", treatment.year),
+             caption = "Treatment effect is interpreted as the share of 2000 forest cover not deforested due to the conservation program.\nA negative effect means the conservation program is associated with higher deforestation.",
+             y = "Avoided deforestation rel. to 2000 (%)") %>%
+      + theme(
+        axis.text.x = element_text(angle = -20, hjust = 0.5, vjust = 0.5),
+        axis.text=element_text(size=11, color = "black"),
+        axis.title=element_text(size=14, color = "black", face = "plain"),
+        
+        plot.caption = element_text(hjust = 0),
+        plot.title = element_text(size=16, color = "black", face = "plain", hjust = 0),
+        plot.subtitle = element_text(size=12, color = "black", face = "plain", hjust = 0),
+        
+        strip.text = element_blank(),
+        
+        #legend.position = "bottom",
+        legend.title = element_blank(),
+        legend.text=element_text(size=10),
+        #legend.spacing.x = unit(1.0, 'cm'),
+        legend.spacing.y = unit(0.75, 'cm'),
+        legend.key.size = unit(2, 'line'),
+        
+        panel.grid.major.x = element_line(color = 'grey80', linewidth = 0.3, linetype = 1),
+        panel.grid.minor.x = element_line(color = 'grey80', linewidth = 0.2, linetype = 2),
+        panel.grid.major.y = element_line(color = 'grey80', linewidth = 0.3, linetype = 1),
+        panel.grid.minor.y = element_line(color = 'grey80', linewidth = 0.2, linetype = 2),
+        
+      )
+    
+    ##Saving plots
+    tmp = paste(tempdir(), "fig", sep = "/")
+    
+    ggsave(paste(tmp, paste0("fig_fc_att_", iso, "_", wdpaid, ".png"), sep = "/"),
+           plot = fig_fc_att,
+           device = "png",
+           height = 6, width = 9)
+    ggsave(paste(tmp, paste0("fig_fl_att_", iso, "_", wdpaid, ".png"), sep = "/"),
+           plot = fig_fl_att,
+           device = "png",
+           height = 6, width = 9)
+    
+    files <- list.files(tmp, full.names = TRUE)
+    ##Add each file in the bucket (same foler for every file in the temp)
+    for(f in files) 
+    {
+      cat("Uploading file", paste0("'", f, "'"), "\n")
+      aws.s3::put_object(file = f, 
+                         bucket = paste("projet-afd-eva-ap", save_dir, iso, wdpaid, sep = "/"),
+                         region = "", show_progress = TRUE)
+    }
+    do.call(file.remove, list(list.files(tmp, full.names = TRUE)))
+    
+    
+    
+    #Return outputs
+    output = list("df_fc_att" = df_fc_attgt, "df_fl_att"  = df_fl_attgt)
+    return(output)
+    
+    
+    
+    
     #TEST : is ATT computed by the did package coherent with manual computations ? 
     # --> YES :D
     # test = df_did %>% 
@@ -248,6 +311,9 @@ fn_did_te = function(iso, wdpaid, df_long_m, alpha, save_dir, ext_output)
     #   mutate(fc_te2 = (avgFC_ha[year == 2009 & group == 2] - avgFC_ha[year == 2006 & group == 2]) - (avgFC_ha[year == 2009 & group == 1] - avgFC_ha[year == 2006 & group == 1]),
     #          fl_te2 = (avgFL_2000_cum[year == 2009 & group == 2] - avgFL_2000_cum[year == 2006 & group == 2]) - (avgFL_2000_cum[year == 2009 & group == 1] - avgFL_2000_cum[year == 2006 & group == 1]))
     # 
+    
+    
+    
 }
   
   
