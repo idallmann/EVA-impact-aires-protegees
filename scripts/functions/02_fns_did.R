fb61e7c60854309a3d6ef46d27763148f7cf99b3
vuillota <vuillota@afd.fr>
2023-08-31 10:15:31 +0000

    Modify did function : compute bootstrap manually ??

--- scripts/functions/02_fns_did.R
+++ scripts/functions/02_fns_did.R
@@ -50,10 +50,9 @@ fn_did_load_df = function(iso, wdpaid, save_dir, ext_output)
 
 }
 
-fn_did_extract_did = function()
 
 #Compute the treatment effect for a given PA
-fn_did_te = function(iso, wdpaid, df_long_m, dt_start, dt_end, yr_T_min, yr_T_max, save_dir, ext_output)
+fn_did_te = function(iso, wdpaid, df_long_m, dt_start, dt_end, yr_T_min, yr_T_max, alpha, save_dir, ext_output)
 {
   
   ####
@@ -220,25 +219,96 @@ fn_did_te = function(iso, wdpaid, df_long_m, dt_start, dt_end, yr_T_min, yr_T_ma
     #Option 2 : utiliser le package did
     ####
     
-    install.packages("did")
-    library(did)
+    #First extract some relevant variables
+    #Extract spatial resolution of pixels res_m and define pixel area in ha
+    res_m = unique(df_long_m$res_m)
+    res_ha = res_m^2*1e-4
     
     df_did = df_long_m %>%
       mutate(treatment_year = case_when(group == 1 ~0,
-                                        group == 2 ~status_yr),
-             .after = status_yr)
+                                        group == 2 ~status_yr), #Set treatment year to 0 for control units (required by did::att_gt)
+             .after = status_yr) %>%
+      group_by(assetid) %>%
+      mutate(FL_2000_cum = (fc_ha-fc_ha[year == 2000])/fc_ha[year == 2000]*100,
+             fper = fc_ha/res_ha*100) %>%
+      ungroup()
     
     
-    pa_attgt = att_gt(yname = "fc_ha",
+    fc_attgt = did::att_gt(yname = "fc_ha",
                       gname = "treatment_year",
                       idname = "assetid",
                       tname = "year",
-                      control_group = "nevertreated",
+                      control_group = "nevertreated", #Thsi corresponds to control pixels as defined in the matching , with treatment year set to 0
                       xformla = ~1,
-                      data = df_did)
+                      alp = alpha, #For 95% confidence interval
+                      allow_unbalanced_panel = TRUE, #Ensure no unit is dropped, though every pixel should have data for all years in the period
+                      bstrap=TRUE, #Compute bootstrap CI
+                      biters = 1000, #The number of bootstrap iteration, 1000 is default
+                      cband = TRUE, #Compute classic CI
+                      clustervars = NULL, #No clustering seems relevant to me (https://blogs.worldbank.org/impactevaluations/when-should-you-cluster-standard-errors-new-wisdom-econometrics-oracle)
+                      base_period = "varying",
+                      data = df_did,
+                      print_details = F)
     
-    summary(pa_attgt)
-    ggdid(pa_attgt)
+    ##Après : 
+    ## Extraire les variables pertinentes
+    
+    df_fc_attgt = data.frame("treatment_year" = fc_attgt$group,
+                             "year" = fc_attgt$t,
+                             "att" = fc_attgt$att,
+                             "se" = fc_attgt$se,
+                             "n" = fc_attgt$n)
+    
+    ##Calculer bootstrap et CI pour les ajouter ! En effet, data::tidy() calcle autrement les CI
+    inffunc = fc_attgt$inffunc
+    n = fc_attgt$n
+    V <- Matrix::t(inffunc) %*% inffunc/n
+    se = sqrt(Matrix::diag(V)/n)
+    se[se <= sqrt(.Machine$double.eps) * 10] <- NA
+    zero_na_sd_entry <- unique(which(is.na(se)))
+    bout <- did::mboot(inffunc, DIDparams = fc_attgt$DIDparams)
+    bres <- bout$bres
+    if (length(zero_na_sd_entry) > 0) {
+      se[-zero_na_sd_entry] <- bout$se[-zero_na_sd_entry]
+    }else {
+      se <- bout$se
+    }
+    se[se <= sqrt(.Machine$double.eps) * 10] <- NA
+    cval <- qnorm(1 - alpha/2)
+    bSigma <- apply(bres, 2, function(b) (quantile(b, 
+                                                   0.75, type = 1, na.rm = T) - quantile(b, 0.25, 
+                                                                                         type = 1, na.rm = T))/(qnorm(0.75) - qnorm(0.25)))
+    bSigma[bSigma <= sqrt(.Machine$double.eps) * 10] <- NA
+    bT <- apply(bres, 1, function(b) max(abs(b/bSigma), 
+                                         na.rm = TRUE))
+    cval <- quantile(bT, 1 - alp, type = 1, na.rm = T)
+    
+    
+    summary(fc_attgt)
+    did::ggdid(fc_attgt)
+    did::process_attgt(fc_attgt)
+    
+    test = did::tidy(fc_attgt)
+    test = fc_attgt$inffunc
+    did::att_gt
+    
+    df_fc_attgt = data.frame("treatment_year" = fc_attgt$group,
+                             "year" = fc_attgt$t,
+                             "att" = fc_attgt$att,
+                             "se" = fc_attgt$se,
+                             "n" = fc_attgt$n)
+    
+    fl_attgt = did::att_gt(yname = "FL_2000_cum",
+                           gname = "treatment_year",
+                           idname = "assetid",
+                           tname = "year",
+                           control_group = "nevertreated",
+                           xformla = ~1,
+                           data = df_did)
+    
+    summary(fl_attgt)
+    did::ggdid(fl_attgt)
+    
     
     
     
@@ -280,4 +350,3 @@ fn_did_te = function(iso, wdpaid, df_long_m, dt_start, dt_end, yr_T_min, yr_T_ma
   
   
   
-}
