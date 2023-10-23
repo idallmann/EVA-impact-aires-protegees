638effe803a16b55eea8d0ab53e699748ec945d6
vuillota <antoine.vuillot@outlook.fr>
2023-10-23 09:16:32 +0000

    Update

--- scripts/functions/02_fns_matching.R
+++ scripts/functions/02_fns_matching.R
@@ -214,17 +214,17 @@ fn_pre_group = function(iso, wdpa_raw, status, yr_min, path_tmp, utm_code, buffe
   # Numerous polygons reported in the WDPA overlap. This can lead be an issue : a pixel can be treated more than once, and the treatment effect of the PA of interest cannot be isolated a priori 
   # Compute a layer of polygons without overlaps, and with overlaps only. The analysis will be performed on polygons with no overlap and whose non-overlapped area is not too small (10% at least of initial area). Also, the overlapped areas are assigned to a specific group so that they are not used as control.
   ## Layer without overlap
-  wdpa_prj_vect = wdpa_prj %>% vect()
+  wdpa_prj_vect = wdpa_prj %>% terra::vect()
   wdpa_prj_noverlap = sapply(1:dim(wdpa_prj_vect)[1], 
                          function(X) {erase(wdpa_prj_vect[X,], wdpa_prj_vect[-X,])}) %>% 
-    vect() %>%
+    terra::vect() %>%
     st_as_sf() %>%
-    mutate(area_overlap = expanse(vect(geometry), unit = "km"),
+    mutate(area_overlap = expanse(terra::vect(geometry), unit = "km"),
            rm = area_overlap < 0.1*REP_AREA)
   ## Layer of overlaps
   wdpa_prj_overlap = sapply(1:dim(wdpa_prj_vect)[1], 
                         function(X) {intersect(wdpa_prj_vect[X,], wdpa_prj_vect[-X,])}) %>% 
-    vect() %>%
+    terra::vect() %>%
     st_as_sf() 
   
   
